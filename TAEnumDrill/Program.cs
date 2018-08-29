a6a4befb2244fb408eb6b77e2d558c23b2ede36d
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-29 14:01:55 -0400

    Enum test

--- TAEnumDrill/Program.cs
+++ TAEnumDrill/Program.cs
@@ -8,8 +8,30 @@ namespace TAEnumDrill
 {
     class Program
     {
+        public enum days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("What day of the week is today?");
+                string userInput = Console.ReadLine();
+                days today = (days)Enum.Parse(typeof(days), userInput); 
+                Console.WriteLine(today);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
         }
     }
 }
